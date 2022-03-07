edc52136bf4bdb465263908db06595bbd3144f51
CKURZ <ckurz098@gmail.com>
2022-03-06 20:22:55 -0800

    added toggles for opacity etc

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const controlContainer = document.getElementById('controlContainer');
 const phosphorsContainer = document.querySelector('.phosphorsContainer');
 phosphorsContainer.style.display = 'block';
+let opacityValue = .9;
+let opacityMask = .7;
+let testImageId = 0;
 
 
 function controlClickHandler(event) {
@@ -17,12 +20,27 @@ function controlClickHandler(event) {
         case 'toggle-b':
             togglePhosphors('blue');
             break;
+        case 'toggle-bloom':
+            toggleDisplay('R-bloom');
+            toggleDisplay('G-bloom');
+            toggleDisplay('B-bloom');
+
+            break;
         case 'toggle-mask':
             toggleDisplay('mask');
             break;
+        case 'opacity-mask':
+            maskOpacity();
+            break;
         case 'opacity':
-            toggleOpacity();
-        break;
+            resetOpacity();
+            break;
+        case 'opacitySet':
+            changeOpacity();
+            break;
+        case 'imageSet':
+            imageSet();
+            break;
     };
 }
 
@@ -37,8 +55,8 @@ function toggleAll() {
     }
 }
 
-function toggleDisplay(color) {
-    const phosphor = document.querySelector(`.${color}`);
+function toggleDisplay(elClass) {
+    const phosphor = document.querySelector(`.${elClass}`);
     if (phosphor.style.display == 'none') {
         phosphor.style.display = 'block'
         event.target.style.color = 'black'
@@ -64,23 +82,61 @@ function togglePhosphors(color) {
     })
 }
 
-function toggleOpacity() {
+function resetOpacity() {
     const elements = document.querySelectorAll('.scanlines');
-    console.log(Object.values(elements))
-
     Object.values(elements).forEach(e => {
         f = e.className.split(' ')[1];
-        console.log(f)
-        if (document.querySelector(`.${f}`).style.opacity == '1') {
-            document.querySelector(`.${f}`).style.opacity = ''
-            event.target.style.color = 'black'
+        document.querySelector(`.${f}`).style.opacity = ''
+        document.querySelector('#opacitySet').innerHTML = 'Change Opacity'
+        opacityValue = .9;
+    })
+}
+
+function changeOpacity() {
+    const elements = document.querySelectorAll('.scanlines');
+    opacityValue += .1;
+    opacityValue = parseFloat(opacityValue.toFixed(2))
+    Object.values(elements).forEach(e => {
+        f = e.className.split(' ')[1];
+        if (opacityValue > 1) {
+            opacityValue = 0;
+            document.querySelector(`.${f}`).style.opacity = `${opacityValue}`;
+            event.target.innerHTML = `Change Opacity ${opacityValue}`
         } else {
-            document.querySelector(`.${f}`).style.opacity = '1'
-            event.target.style.color = 'red'
+            document.querySelector(`.${f}`).style.opacity = `${opacityValue}`;
+            event.target.innerHTML = `Change Opacity ${opacityValue}`
         }
     })
 }
 
+function maskOpacity() {
+    const e = document.querySelector('.mask');
+
+    opacityMask += .1;
+    console.log(opacityMask)
+    opacityMask = parseFloat(opacityMask.toFixed(2));
+
+    if (opacityMask > 1) {
+        opacityMask = 0;
+        e.style.opacity = `${opacityMask}`;
+        event.target.innerHTML = `Mask Opacity ${opacityMask}`
+    } else {
+
+        e.style.opacity = `${opacityMask}`;
+        event.target.innerHTML = `Mask Opacity ${opacityMask}`
+    }
+
+}
+
+function imageSet() {
+    (testImageId === 3) ? testImageId = 0 : testImageId += 1;
+
+    console.log(testImageId);
+    const el = document.querySelector('.test-image');
+
+    el.style.background = `url(./assets/img/image${testImageId}.png) no-repeat center/100vw`
+
+}
 
 
 controlContainer.addEventListener('mouseup', controlClickHandler)
